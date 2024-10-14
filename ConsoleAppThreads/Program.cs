b6a2bf1f1818eced94af208067c05407b8cbd26e
Stanislav Selunin <infleshlightwetrust@gmail.com>
2024-10-14 20:52:01 +0300

    Complete working with threads task

--- ConsoleAppThreads/Program.cs
+++ ConsoleAppThreads/Program.cs
@@ -1,2 +1,90 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.Metrics;
+using System;
+using System.Threading;
+using System.Xml.Serialization;
+using Microsoft.VisualBasic;
+
+
+List<Task> tasks = new List<Task>(50);
+
+for (int i = 1; i < 11; i++)
+{
+    tasks.Add(
+        Task.Factory.StartNew(() =>
+            {
+                Worker.DoWorkNoSync("NO SYNC");
+            })
+    );
+}
+
+Task.WaitAll(tasks.ToArray());
+
+for (int i = 1; i < 11; i++)
+{
+    tasks.Add(
+        Task.Factory.StartNew(() =>
+        {
+            Worker.DoWorkLockSync("LOCK SYNC");
+        })
+    );
+}
+
+Task.WaitAll(tasks.ToArray());
+
+for (int i = 1; i < 11; i++)
+{
+    tasks.Add(
+        Task.Factory.StartNew(() =>
+        {
+            Worker.DoWorkAutoResetEventSync("AutoResetEvent SYNC");
+        })
+    );
+}
+
+Task.WaitAll(tasks.ToArray());
+
+public class Worker
+{
+    private static Random _random = new Random();
+    private static int _counter = 0;
+    static object locker = new();
+    static AutoResetEvent waitHandler = new AutoResetEvent(true);
+    public static void DoWorkNoSync(object msg)
+    {
+        Thread.Sleep(_random.Next(1, 4) * 300);
+        for (var i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Message: {msg}; Counter: {_counter}; Inner iteration number: {i}");
+        }
+        _counter++;
+    }
+
+    public static void DoWorkLockSync(object msg)
+    {
+        lock (locker)
+        {
+            Thread.Sleep(_random.Next(1, 4) * 300);
+            for (var i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Message: {msg}; Counter: {_counter}; Inner iteration number: {i}");
+            }
+            _counter++;
+        }
+    }
+
+    public static void DoWorkAutoResetEventSync(object msg)
+    {
+        waitHandler.WaitOne();
+        Thread.Sleep(_random.Next(1, 4) * 300);
+        for (var i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Message: {msg}; Counter: {_counter}; Inner iteration number: {i}");
+        }
+        _counter++;
+        waitHandler.Set();
+    }
+}
+
+
--- ConsoleAppThreads/obj/Debug/net8.0/ConsoleAppThreads.AssemblyInfo.cs
+++ ConsoleAppThreads/obj/Debug/net8.0/ConsoleAppThreads.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleAppThreads")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+329f8125c66ba7eb8325ac769a4fcbf682ed7572")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleAppThreads")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleAppThreads")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
